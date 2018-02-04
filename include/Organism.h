ba4fa7e8cbd492b594e091b30ddb0c8513589cee
Divix55 <divix55@icloud.com>
2018-02-04 20:57:19 +0100

    Initial commit

---
+++ include/Organism.h
@@ -0,0 +1,17 @@
+//
+// Created by Sandra Wityk on 04.02.2018.
+//
+
+#ifndef INC_2DENGINE_SERVER_ORGANISM_H
+#define INC_2DENGINE_SERVER_ORGANISM_H
+
+
+class Organism {
+    short _health_points;
+public:
+    Organism();
+    virtual ~Organism();
+};
+
+
+#endif //INC_2DENGINE_SERVER_ORGANISM_H
