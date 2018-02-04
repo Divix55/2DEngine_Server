d277fb40416a01b40b3b05519845dac25228a726
Divix55 <divix55@icloud.com>
2018-02-04 21:28:20 +0100

    Add basic getter and setter to Organism class

--- include/Organism.h
+++ include/Organism.h
@@ -1,16 +1,35 @@
 //
-// Created by Sandra Wityk on 04.02.2018.
+// Created by Dawid Walenciak on 04.02.2018.
 //
 
 #ifndef INC_2DENGINE_SERVER_ORGANISM_H
 #define INC_2DENGINE_SERVER_ORGANISM_H
 
 
+#include <cstdint>
+
 class Organism {
-    short _health_points;
+    uint16_t _health_points;        //0 - 65535 for all uint16_t
+    uint16_t _magic_points;
+    uint16_t _shield_points;
+    uint16_t _stamina;
+    uint16_t _capacity;
+
 public:
-    Organism();
+    Organism() : _health_points{0}, _magic_points{0}, _shield_points{0}, _stamina{0}, _capacity{0} {};
     virtual ~Organism();
+
+    uint16_t getHealth_points() const;
+    uint16_t getMagic_points() const;
+    uint16_t getShield_points() const;
+    uint16_t getStamina() const;
+    uint16_t getCapacity() const;
+
+    void setHealth_points(uint16_t health_points);
+    void setMagic_points(uint16_t magic_points);
+    void setShield_points(uint16_t shield_points);
+    void setStamina(uint16_t stamina);
+    void setCapacity(uint16_t capacity);
 };
 
 
--- src/Organism.cpp
+++ src/Organism.cpp
@@ -1,5 +1,45 @@
 //
-// Created by Sandra Wityk on 04.02.2018.
+// Created by Dawid Walenciak on 04.02.2018.
 //
 
 #include "../include/Organism.h"
+
+uint16_t Organism::getHealth_points() const {
+    return _health_points;
+}
+
+void Organism::setHealth_points(uint16_t health_points) {
+    _health_points = health_points;
+}
+
+uint16_t Organism::getMagic_points() const {
+    return _magic_points;
+}
+
+void Organism::setMagic_points(uint16_t magic_points) {
+    _magic_points = magic_points;
+}
+
+uint16_t Organism::getShield_points() const {
+    return _shield_points;
+}
+
+void Organism::setShield_points(uint16_t shield_points) {
+    _shield_points = shield_points;
+}
+
+uint16_t Organism::getStamina() const {
+    return _stamina;
+}
+
+void Organism::setStamina(uint16_t stamina) {
+    _stamina = stamina;
+}
+
+uint16_t Organism::getCapacity() const {
+    return _capacity;
+}
+
+void Organism::setCapacity(uint16_t capacity) {
+    _capacity = capacity;
+}
